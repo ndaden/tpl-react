86ac07bad4f162341e511dd2295ccc5e472c26dc
Nabil DADEN <nabil.daden@hotmail.com>
2019-09-12 19:33:19 +0200

    env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URI = process.env.API_AUTH || 'http://localhost:3000';
+export const API_URI = process.env.API_URI || 'http://localhost:3000';
 export const API_USERS = process.env.API_USERS || '/api/v1/users';
 export const API_AUTH = process.env.API_AUTH || '/api/auth';
